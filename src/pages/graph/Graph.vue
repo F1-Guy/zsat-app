cfee29f7f8dc4ec478c69eb99cf2b350ea3b4d2d
Digna Bringule <dign0009@edu.easj.dk>
2022-12-14 14:37:15 +0100

    Bug fix

--- src/pages/graph/Graph.vue
+++ src/pages/graph/Graph.vue
@@ -7,44 +7,43 @@ import Navbar from "../../components/Navbar.vue";
 import { useUserStore } from "../../lib/store";
 
 export default {
-    setup() {
-        const userStore = useUserStore();
-        return { userStore };
-    },
+  setup() {
+    const userStore = useUserStore();
+    return { userStore };
+  },
 
-    data() {
-        return {
-            attendances: [],
-            today: new Date(),
-            past: new Date(),
-            lookback: 6,
-            graph: null,
-            isLoading: false,
-            color: "#0d6efd",
-        };
-    },
+  data() {
+    return {
+      attendances: [],
+      today: new Date(),
+      past: new Date(),
+      lookback: 6,
+      graph: null,
+      isLoading: false,
+      color: "#0d6efd",
+    };
+  },
 
-    methods: {
-        // Not my proudest code, but hey, it works
-        async extractGraphData(lookbackDays) {
-            let dates = [];
-            let checkinsOnDate = [];
+  methods: {
+    // Not my proudest code, but hey, it works
+    async extractGraphData(lookbackDays) {
+      let dates = [];
+      let checkinsOnDate = [];
 
-            this.past.setDate(this.today.getDate() - lookbackDays);
-            await this.getAttendances();
+      this.past.setDate(this.today.getDate() - lookbackDays);
+      await this.getAttendances();
 
-            for (let i = this.past; i.getTime() <= this.today.getTime(); i.setDate(i.getDate() + 1)) {
-                let dayOfMonth = i.getDate();
-                let counter = 0;
+      for (let i = this.past; i.getTime() <= this.today.getTime(); i.setDate(i.getDate() + 1)) {
+        let dayOfMonth = i.getDate();
+        let counter = 0;
 
-                for (let j = 0; j < this.attendances.length; j++) {
+        for (let j = 0; j < this.attendances.length; j++) {
+          let checkInDate = new Date(this.attendances[j].checkIn).getDate();
 
-                    let checkInDate = new Date(this.attendances[j].checkIn).getDate()
-
-                    if (checkInDate == dayOfMonth) {
-                        counter++;
-                    }
-                }
+          if (checkInDate == dayOfMonth) {
+            counter++;
+          }
+        }
 
         dates.push(dayOfMonth);
         checkinsOnDate.push(counter);
@@ -58,17 +57,17 @@ export default {
       this.attendances = await response.data;
     },
 
-        async getGraph() {
-            this.isLoading = true;
+    async getGraph() {
+      this.isLoading = true;
 
-            if (this.lookback < 0) {
-                this.lookback = 1
-            } else if (this.lookback > 30) {
-                this.lookback = 30
-            }
+      if (this.lookback < 0) {
+        this.lookback = 1;
+      } else if (this.lookback > 30) {
+        this.lookback = 30;
+      }
 
-            let data = await this.extractGraphData(this.lookback);
-            let str = `https://quickchart.io/chart?chart={
+      let data = await this.extractGraphData(this.lookback);
+      let str = `https://quickchart.io/chart?chart={
                         type: 'line',
                         data: {
                             labels: [${data[0]}],
@@ -77,14 +76,13 @@ export default {
                                 data: [${data[1]}],
                                 fill: true}]
                         }}&width=400&height=170&format=svg`;
-            const response = await axios.get(str);
-            this.graph = await response.data;
-            this.isLoading = false;
-        },
+      const response = await axios.get(str);
+      this.graph = await response.data;
+      this.isLoading = false;
+    },
 
-        updateValue(event) {
-            this.lookback = event.target.value;
-        },
+    updateValue(event) {
+      this.lookback = event.target.value;
     },
   },
 
